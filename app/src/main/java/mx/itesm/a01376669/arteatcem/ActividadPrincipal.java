a0bdeeee4b34a18360d4c0dc38295f87480767e8
josemariaaguinigadiaz <A01376669@itesm.mx>
2018-10-16 18:19:24 -0500

    Initial commit

---
+++ app/src/main/java/mx/itesm/a01376669/arteatcem/ActividadPrincipal.java
@@ -0,0 +1,13 @@
+package mx.itesm.a01376669.arteatcem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_principal);
+    }
+}
