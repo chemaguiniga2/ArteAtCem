8f59f63621b1ce01899db75f2c2c7dc87a1385f6
netocervantes <netocervantesvilla@gmail.com>
2018-10-18 15:45:52 -0500

    Prueba Ernesto

--- app/src/main/java/mx/itesm/a01376669/arteatcem/ActividadPrincipal.java
+++ app/src/main/java/mx/itesm/a01376669/arteatcem/ActividadPrincipal.java
@@ -3,6 +3,10 @@ package mx.itesm.a01376669.arteatcem;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
+//Comentario: Ernesto Alejandro Cervantes Villa
+
 public class ActividadPrincipal extends AppCompatActivity {
 
     @Override
